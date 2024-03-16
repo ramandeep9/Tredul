59c4ae2013419b72242e476a9ce1270943381126
ramandeep9 <deepramangill621@gmail.com>
2024-03-16 17:19:02 +0530

    123

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,32 +4,68 @@ import { Link } from 'react-router-dom';
 import Navbar from "./Nav";
  import Footer from "./Footer";
 import './register.css'; 
+import app from "../firebaseConfig";
+import { getDatabase,ref,set,push } from "firebase/database";
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Register: React.FC = () => {
   const [userType, setUserType] = useState<string>(''); // State to store selected user type
+  const [hostname, setInputHname] =useState('');
+  const [hostmail, setInputHmail] =useState('');
+  const [hostpass, setInputHpass] =useState('');
+  const [tourname, setInputTname] =useState('');
+  const [tourmail, setInputTmail] =useState('');
+  const [tourpass, setInputTpass] =useState('');
 
+
+const savehost =() =>{
+  const db = getDatabase(app);
+  const newDoc = push(ref(db, "Eduhosts/profile"));
+  set(newDoc, {
+    hostName: hostname,
+    hostMail: hostmail,
+    hostPass: hostpass
+  }).then(() => {
+    alert("You registered as an Eduhost")
+  }).catch((error)=>{alert('Error')})
+
+}
+
+const savetour =() =>{
+  const db = getDatabase(app);
+  const newDoc = push(ref(db, "Edutourists/profile"));
+  set(newDoc, {
+    tourName: tourname,
+    tourMail: tourmail,
+    tourPass: tourpass
+  }).then(() => {
+    alert("You registered as an Edutourist")
+    // const userCredential = await createUserWithEmailAndPassword
+    // console.log(userCredentail);
+  }).catch((error)=>{
+    alert('Error')})
+}
   const handleUserTypeSelect = (type: string) => {
-    // Popup confirmation alert when user type is selected
+    
+// Popup confirmation alert when user type is selected
     const confirmRegister = window.confirm(`Are you sure you want to register as a ${type}?`);
     if (confirmRegister) {
       setUserType(type);
     }
   };
-
   const renderRegisterForm = () => {
-    // Render register form based on selected user type
-    
+    // Render register form based on selected user type  
     if (userType === 'eduhost') {
-      return (
-        
+      return ( 
         <div className="register-form">
           <div className="register-container">
       <h2 className="me">Create an Account</h2>
-          <input type="texts" placeholder="Host Name" className="usernam" />
-          <input type="emails"className="usernam" placeholder="Email"/> 
-          <input type="passwords" placeholder="Password" className="usernam" />
+          <input onChange={(e) => setInputHname(e.target.value)} type="texts" required  placeholder="Host Name" className="usernam hosteinp" />
+          <input onChange={(e) => setInputHmail(e.target.value)} type="emails" required className="usernam hosteinp" placeholder="Email"/> 
+          <input onChange={(e) => setInputHpass(e.target.value)} type="password" required  placeholder="Password" className="usernam" />
           {/* Add input fields and other necessary elements for eduhost registration */}
-          <button className="mybtn">Register</button>
+          <button onClick={savehost} className="mybtn">Register</button>
+
           <p>Already have an account? <Link to="/login">Login here</Link></p>
       <Link to="/" className="back-link">Go back to Home Page</Link>
         </div></div>
@@ -38,21 +74,22 @@ const Register: React.FC = () => {
       return (
         <div className="register-form">
           <div className="register-container">
-      <h2 className="me">Create an Account</h2>
-          <input type="text_register" id='text_register' placeholder=" Tourist Name" />
-          <input type="email_register" id='email_register' placeholder="Email"/>
-          <input type="password_register" id='password_register'placeholder="Password"/>
+             <h2 className="me">Create an Account</h2>
+          <input onChange={(e) => setInputTname(e.target.value)}  required type="text_register" id='text_register' placeholder=" Tourist Name" />
+          <input onChange={(e) => setInputTmail(e.target.value)}  required type="email_register" id='email_register' placeholder="Email"/>
+          <input onChange={(e) => setInputTpass(e.target.value)}  required type="password" id='password_register'placeholder="Password"/>
           {/* Add more input fields as needed */}
-          <button className="mybtn">Register</button>
+          <button onClick={savetour} className="mybtn">Register</button>
+
           <p>Already have an account? 
             <Link to="/login">Login here</Link></p>
-      <Link to="/" className="back-link">Go back to Home Page</Link>
-        </div></div>
+            <Link to="/" className="back-link">Go back to Home Page</Link>
+             </div></div>
       );
-    } else {
-      return null; // Render nothing if no user type is selected
-    }
-  };
+       } else {
+        return null; // Render nothing if no user type is selected
+      }
+   }
 
   return (
     <div><Navbar/>
---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey:"AIzaSyA9Pf0qMYVyosv9QXQl5QzHTq470vmpNhs",
+    authDomain:"tredul.firebaseapp.com",
+    databaseURL: "https://tredul-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tredul",
+    storageBucket:"tredul.appspot.com",
+    messagingSenderId: "790353321744",
+    appId:"1:790353321744:web:d6cb70d224cf3406558f95",
+    measurementId: "G-2SP1378XTN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth()
+export default app;
